57ccd0d544704ea30df4982e196f9e75e52aefe1
Natalia Brenda Andino <nandinoatencio@gmail.com>
2023-12-01 12:03:14 -0300

    versión 5 corregida

--- spring-boot-jwt-authentication-main - v5CORREGIDA/spring-boot-jwt-authentication-main/src/main/java/com/brenA/demojwt/seguridad/auth/AuthService.java
+++ spring-boot-jwt-authentication-main - v5CORREGIDA/spring-boot-jwt-authentication-main/src/main/java/com/brenA/demojwt/seguridad/auth/AuthService.java
@@ -18,29 +18,19 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 
         private final UserRepository userRepository;
-        private final PasswordEncoder passwordEncoder;
         private final JwtService jwtService;
-        // declaramos un AuthenticarionManager para administrar los filtros del login
+        private final PasswordEncoder passwordEncoder;
         private final AuthenticationManager authenticationManager;
 
-        // recibimos por parámetro los datos del login
         public AuthResponse login(LoginRequest request) {
-                // para autenticar el usuario, usamos este método authenticate
-                // instanciamos un nuevo UsernamePasswordAuthenticationToken con el nombre y
-                // contraseña procesado en la solicitud,
-                // este objeto trabajará en conjunto con el AuthenticationProvider para validar
-                // la solicitud
-                // en caso de éxito, avanza hacia la generación del token
-                authenticationManager
-                                .authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(),
-                                                request.getPassword()));
-                // para generar el token, Security precisa trabajar con el UserDetails,
-                // que tiene detalles de la sesión que también pasan por un filtro de seguridad
+                authenticationManager.authenticate(
+                                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
                 UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
                 String token = jwtService.getToken(user);
                 return AuthResponse.builder()
                                 .token(token)
                                 .build();
+
         }
 
         public AuthResponse register(RegisterRequest request) {
--- spring-boot-jwt-authentication-main - v5CORREGIDA/spring-boot-jwt-authentication-main/src/main/java/com/brenA/demojwt/seguridad/config/ApplicationConfig.java
+++ spring-boot-jwt-authentication-main - v5CORREGIDA/spring-boot-jwt-authentication-main/src/main/java/com/brenA/demojwt/seguridad/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.brenA.demojwt.seguridad.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,26 +19,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-    @Autowired
     private final UserRepository userRepository;
 
     @Bean
-    // para configurar el AuthenticationManager, usamos la implementación
-    // AutenticationConfiguration, enviada por parámetro (pertence a Spring)
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
     @Bean
-    // el Manager se conecta con el Provedor de accesos, que en este caso, es del
-    // tipo
-    // Data Acces Object
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        // nos pide un UserDetailsService, que pondremos en funcionamiento
-        // con un método homónimo
         authenticationProvider.setUserDetailsService(userDetailService());
-        // nos pide saber con qué método estamos encriptando las contraseñas
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
@@ -52,7 +42,7 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailService() {
         return username -> userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not fournd"));
     }
 
 }
